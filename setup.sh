30809adc4d55868b0f4757be67a91b90ba73096f
Marco Biedermann <marco.biedermann@gmx.de>
2015-10-20 17:26:39 +0200

    add setup file to create basic folder structure

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir ~/Documents/Projects
+mkdir ~/Downloads/Install
+mkdir ~/Downloads/Resources
+mkdir ~/Downloads/Resources/Fonts
+mkdir ~/Downloads/Resources/Icons
+mkdir ~/Downloads/Resources/Images
