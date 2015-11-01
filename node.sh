c0d049f7ad663472dfd7c0d13d2644913ffdc481
Marco Biedermann <marco.biedermann@gmx.de>
2015-11-01 22:30:27 +0100

    add node.js setup file

---
+++ node.sh
@@ -0,0 +1,11 @@
+# Ask for the administrator password upfront.
+sudo -v
+
+# Install Node Modules globally
+npm install babel-cli -g
+npm install bower -g
+npm install csslint -g
+npm install grunt-cli -g
+npm install gulp -g
+npm install jscs -g
+npm install jshint -g
