183301999a21c0901a3fc86e086ce785cb5b7bd4
Marco Biedermann <marco.biedermann@gmx.de>
2016-02-28 23:11:12 +0100

    fix typo

--- Applications/Sublime Text 3/setup.sh
+++ Applications/Sublime Text 3/setup.sh
@@ -7,7 +7,7 @@ then
 	wget https://packagecontrol.io/Package%20Control.sublime-package -P ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
 fi
 
-# Symlinc Settings
+# Symlink Settings
 mkdir ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 rm -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 ln -s Packages/User/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
