cd5284a3ba4434e912d05a163c0a4b9552931971
Marco Biedermann <marco.biedermann@gmx.de>
2017-10-01 23:19:26 +0200

    add airbnb eslint config

--- node.sh
+++ node.sh
@@ -8,3 +8,7 @@ npm install -g greenkeeper
 npm install -g jscs
 npm install -g postcss
 npm install -g stylelint
+(
+  export PKG=eslint-config-airbnb;
+  npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install -g "$PKG@latest"
+)
