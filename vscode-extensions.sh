b607e8f69930df4a87b297099e729fa8fb927180
Marco Biedermann <marco.biedermann@gmx.de>
2017-10-06 00:44:59 +0200

    add installed vscode extensions

---
+++ vscode-extensions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dkundel.vscode-new-file
+code --install-extension EditorConfig.EditorConfig
+code --install-extension felixfbecker.php-intellisense
+code --install-extension ikappas.phpcs
+code --install-extension mrmlnc.vscode-scss
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension shinnn.stylelint
+code --install-extension WallabyJs.quokka-vscode
+code --install-extension Zignd.html-css-class-completion
