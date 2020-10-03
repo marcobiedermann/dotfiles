8aa04ea83cffdab4592d33726c58134d7bffa73f
Marco Biedermann <marco.biedermann@gmx.de>
2020-10-03 20:24:03 +0200

    feat: Setup
    Update setup

--- setup.sh
+++ setup.sh
@@ -1,63 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/Documents/Projects
-mkdir -p ~/Documents/Projects/Forks
-mkdir -p ~/Documents/Projects/Open\ Source
-
-mkdir -p ~/Downloads/Install
-
-mkdir -p ~/Downloads/Resources/Fonts
-mkdir -p ~/Downloads/Resources/Icons
-mkdir -p ~/Downloads/Resources/Images
-
-mkdir -p /data/db
-
-# XCode Command Line Tools
-# thx https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
-
-if ! xcode-select --print-path &> /dev/null; then
-
-    # Prompt user to install the XCode Command Line Tools
-    xcode-select --install &> /dev/null
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Wait until the XCode Command Line Tools are installed
-    until xcode-select --print-path &> /dev/null; do
-        sleep 5
-    done
-
-    print_result $? 'Install XCode Command Line Tools'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Point the `xcode-select` developer directory to
-    # the appropriate directory from within `Xcode.app`
-    # https://github.com/alrra/dotfiles/issues/13
-
-    sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-    print_result $? 'Make "xcode-select" developer directory point to Xcode'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Prompt user to agree to the terms of the Xcode license
-    # https://github.com/alrra/dotfiles/issues/10
-
-    sudo xcodebuild -license
-    print_result $? 'Agree with the XCode Command Line Tools licence'
-
-fi
-
-
 # Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install Tools
-sh brew.sh
-sh brew-cask.sh
-sh gems.sh
-sh node.sh
+sh Brewfile
 sh .osx
