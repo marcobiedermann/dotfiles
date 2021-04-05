cc093056f70ad665db9e63c7d67c8e2bde56db79
Marco Biedermann <marco.biedermann@gmx.de>
2021-04-05 12:19:27 +0200

    feat: Applications
    Update applications in Brewfile

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Tools
-sh Brewfile
+brew bundle
 sh .osx
